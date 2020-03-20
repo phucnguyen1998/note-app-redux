ab7f0fa5dc4f909126d5072bde1c882d8f7db93d
phucnguyen1998 <phucnguyen11011998@gmail.com>
2020-03-20 09:28:43 +0700

    Chuc nang them ghi chu

--- src/store.js
+++ src/store.js
@@ -4,7 +4,8 @@ let redux = require('redux');
 
 const noteInitialState = {
     isEdit: false,
-    editNote: {}
+    editNote: {},
+    isAdd: false
 };// trang thai ban dau cua state
 //reducer quan ly state va action 
 const allReducer = (state = noteInitialState, action) => {
@@ -13,26 +14,34 @@ const allReducer = (state = noteInitialState, action) => {
             //console.log(action.getItem);
             noteData.push(action.getItem);
             return state
+
         case "SHOW_EDIT_FORM":
-            return {...state,isEdit:!state.isEdit}   
+            return {...state,isEdit:!state.isEdit} 
+
+        case "CHANGE_TITLE_FORM":
+            return {...state,isAdd:!state.isAdd,editNote:{}} 
+
         case "GET_EDIT_DATA":
             return {...state,editNote:action.editNote}
+
         case "EDIT":
             noteData.child(action.getItem.key).update({
                 noteTitle: action.getItem.noteTitle,
                 noteContent: action.getItem.noteContent
             });
             return {...state,editNote:{}}
+
         case "DELETE_DATA":
             noteData.child(action.isDelete.key).remove();
             return state
+
         default:
             return state
     }
 }
 
 let store = redux.createStore(allReducer); //store quan ly reducer
-// store.subscribe(function(){
-//     console.log("subscribe",JSON.stringify(store.getState()));
-// })
+store.subscribe(function(){
+    console.log("subscribe",JSON.stringify(store.getState()));
+})
 export default store;
